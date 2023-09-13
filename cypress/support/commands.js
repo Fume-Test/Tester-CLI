8116f34ec9e07108b12bfd746a81eabb18af0ccd
aegucer <43250844+emregucerr@users.noreply.github.com>
2023-09-13 11:35:02 +0300

    replay user action commmand

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -62,3 +62,41 @@ Cypress.Commands.add('findTarget', (xpath, tagName, id = null, innerHTML = null,
     // If the element wasn't found by any method, return null
     return null;
 });
+
+Cypress.Commands.add('recreateUserAction', (userEvent) => {
+    // Extract the required details from the user event object
+    const { eventType, detail } = userEvent;
+    const { id, xPath, tag_name, inner_HTML, placeholder, value } = detail;
+
+    // Locate the target element using the findTarget command
+    cy.findTarget(xPath, tag_name, id, inner_HTML, placeholder).then($el => {
+        // Check if the element is found
+        if ($el) {
+            switch (eventType) {
+                case 'focusout':
+                    $el.trigger('focusout');
+                    break;
+                case 'input':
+                    // For input events, update the value first and then trigger the event
+                    $el.val(value).trigger('input');
+                    break;
+                case 'wheel':
+                    // Trigger the wheel event
+                    $el.trigger('wheel');
+                    break;
+                case 'click':
+                    // Trigger the click event
+                    $el.click();
+                    break;
+                case 'dblclick':
+                    // Trigger the double-click event
+                    $el.dblclick();
+                    break;
+                default:
+                    cy.log(`Unknown event type: ${eventType}`);
+            }
+        } else {
+            cy.log('Element not found');
+        }
+    });
+});
